bcc8a19e8bcdfc3f4abae56ae028d0778ae8b4a8
Siegfried Gessulat <siegfried.gessulat@sap.com>
2015-11-12 11:38:38 +0100

    cleaned js and added data munging dir

--- js/init.js
+++ js/init.js
@@ -1,9 +1,13 @@
-      function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(52.529208, 13.378254),
-          zoom: 13,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map_canvas"),
-            mapOptions);
-      }
+var locations = {
+    bmvi: [52.529208, 13.378254]
+};
+
+function initialize() {
+    var mapOptions = {
+        center: new google.maps.LatLng(locations.bmvi[0], locations.bmvi[1]),
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"),
+        mapOptions);
+}
